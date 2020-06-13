a7aefb73cd983d1c30f59335761feed85e2dac1d
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2020-06-13 12:14:28 +0900

    domjudge_package: Update

--- contest/experimental_defs.bzl
+++ contest/experimental_defs.bzl
@@ -1,17 +1,24 @@
-def domjudge_package(name, metadata, dataset, statements = [], **kwargs):
+def domjudge_package(name, dataset, domjudge_metadata = None, icpc_metadata = None, statements = [], **kwargs):
     out = name + ".zip"
     args = [
         "'$(execpath @rules_contest//contest/impls:domjudge_package)'",
         "--output='$@'",
-        "--metadata='$(execpath " + metadata + ")'",
         "--dataset='$(execpath " + dataset + ")'",
     ]
+    srcs = [dataset]
+    if domjudge_metadata:
+        args.append("--domjudge_metadata='$(execpath " + domjudge_metadata + ")'")
+        srcs.append(domjudge_metadata)
+    if icpc_metadata:
+        args.append("--icpc_metadata='$(execpath " + icpc_metadata + ")'")
+        srcs.append(icpc_metadata)
     for statement in statements:
         args.append("--statement='$(execpath " + statement + ")'")
+        srcs.append(statement)
     native.genrule(
         name = name,
         outs = [out],
-        srcs = [metadata, dataset] + statements,
+        srcs = srcs,
         tools = ["@rules_contest//contest/impls:domjudge_package"],
         cmd = " ".join(args),
         **kwargs
--- contest/impls/domjudge_package.py
+++ contest/impls/domjudge_package.py
@@ -11,18 +11,24 @@ from contest.impls.lib import datasets
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--output', required=True)
-    parser.add_argument('--metadata', required=True)
-    parser.add_argument('--statements', action='append')
+    parser.add_argument('--domjudge_metadata')
+    parser.add_argument('--icpc_metadata')
+    parser.add_argument('--statements', action='append', default=[])
     parser.add_argument('--dataset', required=True)
     options = parser.parse_args()
 
     # DOMJudge problem format:
     # https://www.domjudge.org/docs/manual/problem-format.html
     with tempfile.TemporaryDirectory() as tmp_dir:
-        with open(options.metadata, 'r') as f:
-            # Make sure the metadata YAML is well-formed.
-            yaml.safe_load(f)
-        shutil.copy(options.metadata, os.path.join(tmp_dir, 'problem.yaml'))
+        if options.domjudge_metadata:
+            shutil.copy(
+                options.domjudge_metadata,
+                os.path.join(tmp_dir, 'domjudge-problem.ini'))
+
+        if options.icpc_metadata:
+            shutil.copy(
+                options.icpc_metadata,
+                os.path.join(tmp_dir, 'problem.yaml'))
 
         for statement in options.statements:
             assert statement.endswith(('.html', '.pdf'))
